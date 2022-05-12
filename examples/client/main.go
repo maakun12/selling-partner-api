932595b67c2ebc6b43a2bb689e47efaabc3ed295
maakun12 <amram.system@gmail.com>
2022-05-12 22:31:04 +0900

    chenge filename&test

--- examples/client/main.go
+++ examples/client/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/maakun12/selling-partner-api/spapi"
 )
 
 func main() {
 	c, err := spapi.NewClient(&spapi.Config{
-		RefreshToken: "<RefreshToken>",
-		ClientID:     "<ClientID>",
-		ClientSecret: "<ClientSecret>",
-		AccessKey:    "<AccessKey>",
-		SecretKey:    "<SecretKey>",
+		RefreshToken: os.Getenv("LWA_REFRESH_TOKEN"),
+		ClientID:     os.Getenv("LWA_CLIENT_ID"),
+		ClientSecret: os.Getenv("LWA_CLIENT_SECRET"),
+		AccessKey:    os.Getenv("AWS_ACCESS_KEY"),
+		SecretKey:    os.Getenv("AWS_SECRET_KEY"),
 	})
 	if err != nil {
 		panic(err)
